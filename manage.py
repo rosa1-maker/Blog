e1a8fd925a218fc3dfb0320a67f0e6b671cb331f
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-20 12:08:46 +0300

    created a navbar in index.html f

--- manage.py
+++ manage.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
-    render_template('index.html')
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+@app.route('/contacts')  
+def contacts():
+    return render_template('contacts.html')
+ 
 if __name__== '__main__':
     app.run(debug=True)
